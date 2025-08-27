f020cfe32f1d2489eef13598d1549bc95bc58c2c
Gustavo Rodríguez <ga.rodriguezh@uea.edu.ec>
2025-08-26 21:10:24 -0500

    Optimización del Código

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace TraductorBasico
+﻿public class TraductorBasico
 {
     class Traductor
     {
--- obj/Debug/net8.0/Traductor_Basico.AssemblyInfo.cs
+++ obj/Debug/net8.0/Traductor_Basico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traductor_Basico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe4c35e7d434eb5a9647874a87eb32272ce22d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16725694e3d17e734cd366384900d6d22614583")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traductor_Basico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traductor_Basico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
