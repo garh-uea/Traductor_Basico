ae42050d1781fe5c95487c60fec0baad38648fa5
Gustavo Rodríguez <ga.rodriguezh@uea.edu.ec>
2025-09-27 14:38:40 -0500

    Programa corregido según observaciones dadas por el Profesor

--- obj/Debug/net8.0/Traductor_Basico.AssemblyInfo.cs
+++ obj/Debug/net8.0/Traductor_Basico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Traductor_Basico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d68a6275c2e4cae024430fd8aa05336caf3dd82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74d627091fe2f758f60d641a60c27bfa0806501a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Traductor_Basico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Traductor_Basico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
